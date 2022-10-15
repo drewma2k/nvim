f0c63cdc4e82999b76b35788985209ff600df487
Drew Abram <drew.abram@gmail.com>
2022-10-15 09:51:15 -0400

    fix plugin breaking changes; fix lua hover syntax issue

--- lua/config/plugin/lspconfig.lua
+++ lua/config/plugin/lspconfig.lua
@@ -54,7 +54,7 @@ end
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- override individual language server options with a file that
 -- returns the options table
