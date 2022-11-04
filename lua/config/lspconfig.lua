ce81ca2debfea56876154f899eb8085de7a51814
Drew Abram <drew.abram@gmail.com>
2022-11-04 16:47:44 -0400

    keymap: quickfix containing all project diagnostics

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -23,7 +23,8 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<space>ll', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<space>qq', vim.diagnostic.setqflist, opts)
 
 -- LSP maps
 local on_attach = function(_, bufnr)
