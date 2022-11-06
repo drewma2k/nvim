2592fb0dd17120c3fccac3dea6351d614cbde76a
Drew Abram <drew.abram@gmail.com>
2022-11-06 10:24:37 -0500

    improve default colorscheme

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -3,5 +3,11 @@ if not ok then
 	return
 end
 
+-- better default highlight groups
+vim.cmd[[hi Pmenu ctermbg=darkgrey ctermfg=white]]
+vim.cmd[[hi PmenuSel ctermbg=grey ctermfg=lightgrey]]
+vim.cmd[[hi ColorColumn ctermbg=darkgrey]]
+vim.cmd[[hi clear SignColumn]]
+
 --Set colorscheme
 vim.cmd [[colorscheme base16-eighties]]
