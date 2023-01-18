5ea3cb82b3c0383247e69e385144f0a6e95a11ac
Drew Abram <drew.abram@gmail.com>
2023-01-18 11:29:35 -0500

    nvim api completion source

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -76,6 +76,7 @@ return require('packer').startup(function(use)
 	use 'hrsh7th/cmp-path'
 	use 'hrsh7th/cmp-cmdline'
 	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cmp-nvim-lua'
 	use 'hrsh7th/cmp-nvim-lsp-signature-help'
 
 	use 'L3MON4D3/LuaSnip'
