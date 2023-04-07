88de2428380f48d7ca6d281a7a10b66233ef5663
Drew Abram <drew.abram@gmail.com>
2023-04-07 17:02:02 -0400

    statuscol config

---
+++ lua/config/statuscol.lua
@@ -0,0 +1,39 @@
+local ok, statuscol = pcall(require, 'statuscol')
+if not ok then
+	return
+end
+local ok, builtin = pcall(require, 'statuscol.builtin')
+if not ok then
+	return
+end
+
+statuscol.setup({
+	ft_ignore = {
+		'NvimTree', 'help', 'alpha', 'Trouble', 'mason'
+	},
+	segments = {
+		{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+		{
+			click = "v:lua.ScSa",
+			sign = {
+				name = { "Dap", "Diagnostic" },
+				condition = { true, builtin.not_empty },
+				click = "v:lua.ScLa",
+			}
+		},
+		{
+			text = { builtin.lnumfunc, " " },
+			condition = { true, builtin.not_empty },
+			click = "v:lua.ScLa",
+		},
+		{
+			sign = {
+				name = { 'GitSigns' },
+				maxwidth = 1,
+				colwidth = 1,
+				auto = false,
+			},
+			click = 'v:lua.ScSa'
+		}
+	},
+})
