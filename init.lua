f1df1beacaf9576440a056828adbbdf794b592ca
Drew Abram <drew.abram@gmail.com>
2024-12-20 14:59:19 -0500

    refactor dap and treesitter

--- init.lua
+++ init.lua
@@ -5,7 +5,6 @@ require('config.key-mapping')
 
 -- setup plugins
 require('config.cmp')
-require('config.dap')
--- require('config.marks')
 require('config.cmp')
+-- require('config.marks')
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,7 +1,76 @@
 return {
-	{'mfussenegger/nvim-dap', dependencies = "nvim-neotest/nvim-nio"},
-	{'rcarriga/nvim-dap-ui'},
-	{'leoluz/nvim-dap-go'},
+	{
+		'mfussenegger/nvim-dap',
+		dependencies = { "nvim-neotest/nvim-nio", "rcarriga/nvim-dap-ui", "mfussenegger/nvim-dap-python" },
+		config = function()
+			local ok, dap = pcall(require, 'dap')
+			if not ok then
+				return
+			end
+
+			local ok, dap_go = pcall(require, 'dap-go')
+			if not ok then
+				return
+			end
+
+			local dapui = require('dapui')
+
+			dapui.setup()
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+
+			dap_go.setup()
+
+			vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = 'red' })
+			vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DapBreakpoint', linehl = '', numhl = '' })
+
+			-- go configuration
+			table.insert(dap.configurations.go, {
+				type = "go",
+				name = "Debug cmd/server",
+				request = "launch",
+				program = "${workspaceFolder}/cmd/server"
+			})
+
+			table.insert(dap.configurations.go, {
+				type = "go",
+				name = "Debug $MAINPATH",
+				request = "launch",
+				program = function()
+					local co = coroutine.running()
+					local cb = function(input)
+						coroutine.resume(co, input)
+					end
+					cb = vim.schedule_wrap(cb)
+					vim.ui.input({ "Enter path to main.go: ", kind = 'dap' }, cb)
+					local path = coroutine.yield()
+					return "${workspaceFolder}/" .. path
+				end
+				-- program = function ()
+				-- 	return "${workspaceFolder}"..vim.fn.input("Enter path to main.go: ")
+				-- end;
+				-- program = "${workspaceFolder}${MAINPATH}"
+			})
+
+			--  attach remote
+			table.insert(dap.configurations.go, {
+				type = "go",
+				name = "Attach remote",
+				mode = "local",
+				request = "attach",
+				processId = require('dap.utils').pick_process,
+			})
+		end
+	},
+	{ 'rcarriga/nvim-dap-ui' },
+	{ 'leoluz/nvim-dap-go' },
 	{
 		"mfussenegger/nvim-dap-python",
 		config = function()
