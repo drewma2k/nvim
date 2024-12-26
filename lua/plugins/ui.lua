ec2553c6654b0fa9a17fc023e13701f5bfbd1077
Drew Abram <drew.abram@gmail.com>
2024-12-26 11:36:17 -0500

    stop treesitter cluttering messages

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -662,7 +662,7 @@ return {
 			local configs = require("nvim-treesitter.configs")
 
 			configs.setup({
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python" },
+				-- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python" },
 				sync_install = false,
 				highlight = { enable = true },
 				indent = { enable = true },
