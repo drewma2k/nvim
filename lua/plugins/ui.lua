647a78f36171b44991d27090f11326d0f3b682a1
Drew Abram <drew.abram@gmail.com>
2024-12-27 09:15:07 -0500

    remove some UI plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -570,87 +570,6 @@ return {
 		}
 	},
 	{
-		"folke/noice.nvim",
-		enabled = false,
-		event = "VeryLazy",
-		opts = {
-			routes = {
-				{
-					view = "mini",
-					filter = { event = "msg_showmode" }
-				}
-			},
-			-- add any options here
-			lsp = {
-				signature = {
-					enabled = false
-				},
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-				}
-			},
-			presets = {
-				-- bottom_search = true, -- use a classic bottom cmdline for search
-				-- command_palette = true, -- position the cmdline and popupmenu together
-				long_message_to_split = true, -- long messages will be sent to a split
-				command_palette = false, -- position the cmdline and popupmenu together
-				-- inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = true, -- add a border to hover docs and signature help
-			},
-			cmdline = {
-				enabled = true,
-				view = "cmdline",
-				format = {
-					cmdline = false,
-					search_down = false,
-					search_up = false,
-					filter = false,
-					lua = false,
-					help = false,
-					input = false
-				}
-			},
-			messages = {
-				enabled = true,
-				view = "mini",
-			},
-			popupmenu = {
-				enabled = false
-			},
-			notify = {
-				enabled = true,
-				view = "mini"
-			},
-			hover = {
-				silent = true
-			},
-			views = {
-				mini = {
-					position = {
-						row = -2
-					}
-				},
-				notify = {
-					style = "minimal"
-				}
-			}
-		},
-		config = function(self)
-			require('noice').setup(self.opts)
-			vim.notify("noice.nvim loaded", "info", { title = "noice.nvim" })
-		end,
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-		}
-	},
-	{
 		'nvim-treesitter/nvim-treesitter',
 		-- version = "v0.9.0",
 		build = ":TSUpdate",
@@ -689,14 +608,4 @@ return {
 			}
 		}
 	},
-	{
-		"nvim-telescope/telescope-file-browser.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-	},
-	{
-		"rcarriga/nvim-notify",
-		opts = {
-			stages = "static"
-		}
-	}
 }
