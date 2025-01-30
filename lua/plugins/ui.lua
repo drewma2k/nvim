b0f17eb1c4b15074c64daf33f94aaf289cd76694
Drew Abram <drew.abram@gmail.com>
2025-01-30 06:48:29 -0500

    dsable more of noice

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -31,6 +31,9 @@ return {
 					progress = {
 						enabled = false
 					},
+					signature = {
+						enabled = false
+					},
 					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 					override = {
 						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
