6351b54ca22bb156d81b410292dbf04a6ef7be3b
Drew Abram <drew.abram@gmail.com>
2025-01-30 06:15:51 -0500

    noice work in progress

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,5 +1,55 @@
 return {
 	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+		config = function()
+			require("noice").setup({
+				cmdline = {
+					enabled = false
+				},
+				messages = {
+					enabled = false
+				},
+				popupmenu = {
+					enabled = false
+				},
+				notify = {
+					enabled = false
+				},
+				lsp = {
+					progress = {
+						enabled = false
+					},
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						-- ["cmp.entry.get_documentation"] = false, -- requires hrsh7th/nvim-cmp
+					},
+				},
+				-- you can enable a preset for easier configuration
+				presets = {
+					bottom_search = false, -- use a classic bottom cmdline for search
+					command_palette = false, -- position the cmdline and popupmenu together
+					long_message_to_split = false, -- long messages will be sent to a split
+					inc_rename = false, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+			})
+		end,
+	},
+	{
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {}
@@ -683,7 +733,7 @@ return {
 			defaults = {
 				mappings = {
 					i = {
-						["<C-q>"] = require('telescope.actions').smart_send_to_qflist
+						["<C-q>"] = "smart_send_to_qflist"
 					}
 				},
 				file_ignore_patterns = {
