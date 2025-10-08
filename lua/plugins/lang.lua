6c96588a22c59d9da18427b8e4d72d98b674a6b3
Drew Abram <drew.abram@gmail.com>
2025-10-08 18:06:22 -0400

    obsidian-nvim, mdx support

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -1,5 +1,45 @@
 return {
 	{
+		"obsidian-nvim/obsidian.nvim",
+		version = "*", -- recommended, use latest release instead of latest commit
+		ft = "markdown",
+		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+		-- event = {
+		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+		--   -- refer to `:h file-pattern` for more examples
+		--   "BufReadPre path/to/my-vault/*.md",
+		--   "BufNewFile path/to/my-vault/*.md",
+		-- },
+		---@module 'obsidian'
+		---@type obsidian.config
+		opts = {
+			completion = {
+				-- Enables completion using nvim_cmp
+				nvim_cmp = false,
+				-- Enables completion using blink.cmp
+				blink = true,
+			},
+			workspaces = {
+				{
+					name = "notes",
+					path = "~/Documents/notes",
+				},
+				-- {
+				--   name = "work",
+				--   path = "~/vaults/work",
+				-- },
+			},
+
+			-- see below for full list of options 👇
+		},
+	},
+	{
+		"davidmh/mdx.nvim",
+		config = true,
+		dependencies = { "nvim-treesitter/nvim-treesitter" }
+	},
+	{
 		"mistweaverco/kulala.nvim",
 		dependencies = {
 			"nvim-tree/nvim-web-devicons"
@@ -95,7 +135,7 @@ return {
 				executable = "mvn",
 				commands = {
 					{ cmd = { "install", "-Dmaven.test.skip" }, desc = "Install without tests" },
-					{ cmd = {"clean", "install"}, desc = "Clean Install"}
+					{ cmd = { "clean", "install" },             desc = "Clean Install" }
 				}
 			})
 		end
