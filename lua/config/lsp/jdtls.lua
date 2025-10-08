fa023d17dfbd6019eef8e6e81b60a3f253ba3b9c
Drew Abram <drew.abram@gmail.com>
2025-10-08 18:17:17 -0400

    disable onCompletionItemSelectedCommand again

--- lua/config/lsp/jdtls.lua
+++ lua/config/lsp/jdtls.lua
@@ -1,9 +1,44 @@
 return {
 	-- overwrites options from nvim-java. No way to merge yet.
 	-- Either have to copy config from nvim-java or don't edit init_options
-	-- init_options = {
-	-- 	bundles = { vim.env.HOME .. "/.local/share/nvim/mason/packages/share/jdtls/plugins/com.microsoft.java.maven.jar"},
-	-- },
+	init_options = {
+		-- 	bundles = { vim.env.HOME .. "/.local/share/nvim/mason/packages/share/jdtls/plugins/com.microsoft.java.maven.jar"},
+		-- 	must overwrite extended capabilities in order to disable onCompletionItemSelectedCommand
+		extendedClientCapabilities = {
+			actionableRuntimeNotificationSupport = true,
+			advancedExtractRefactoringSupport = true,
+			advancedGenerateAccessorsSupport = true,
+			advancedIntroduceParameterRefactoringSupport = true,
+			advancedOrganizeImportsSupport = true,
+			advancedUpgradeGradleSupport = true,
+			classFileContentsSupport = true,
+			clientDocumentSymbolProvider = true,
+			clientHoverProvider = false,
+			executeClientCommandSupport = true,
+			extractInterfaceSupport = true,
+			generateConstructorsPromptSupport = true,
+			generateDelegateMethodsPromptSupport = true,
+			generateToStringPromptSupport = true,
+			gradleChecksumWrapperPromptSupport = true,
+			hashCodeEqualsPromptSupport = true,
+			inferSelectionSupport = {
+				'extractConstant',
+				'extractField',
+				'extractInterface',
+				'extractMethod',
+				'extractVariableAllOccurrence',
+				'extractVariable',
+			},
+			moveRefactoringSupport = true,
+			-- This option causes issues with blink.cmp, because the
+			-- implemented function does not return a value. Must override the
+			-- whole table because nvim-java does not deep-extend this config.
+			-- nvim-java issue: https://github.com/nvim-java/nvim-java/issues/396
+			-- blink.cmp issue: https://github.com/Saghen/blink.cmp/issues/1864
+			-- onCompletionItemSelectedCommand = 'editor.action.triggerParameterHints',
+			overrideMethodsPromptSupport = true,
+		},
+	},
 	settings = {
 		java = {
 			eclipse = {
