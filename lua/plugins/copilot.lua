a881b466d53bbf06b622572d79278fb7920055a6
Drew Abram <drew.abram@gmail.com>
2025-10-09 07:48:42 -0400

    fix codecompanion warning

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -55,18 +55,21 @@ return {
 			"nvim-lua/plenary.nvim",
 			"nvim-treesitter/nvim-treesitter",
 		},
-		init = function ()
+		init = function()
 			-- vim.keymap.set({"n", "v"}, "<C-a>", "<cmd>CodeCompanionActions<cr>", {noremap=true, silent=true})
-			vim.keymap.set({"n", "v"}, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", {noremap=true, silent=true})
-			vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", {noremap=true, silent=true})
+			vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>",
+				{ noremap = true, silent = true })
+			vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
 			vim.cmd([[cab cc CodeCompanion]])
 		end,
 		opts = {
 			adapters = {
-				opts = {
-					allow_insecure = true,
-					-- proxy = ""
-				}
+				http = {
+					opts = {
+						allow_insecure = true,
+						-- proxy = ""
+					}
+				},
 			},
 			display = {
 				chat = {
