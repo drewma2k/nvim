27c731d5f1614c7859e869a604cc18d4c1522d86
Drew Abram <drew.abram@gmail.com>
2025-10-09 07:24:47 -0400

    add molten and  jupytext for notebooks

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -1,5 +1,17 @@
 return {
 	{
+		"goerz/jupytext.nvim",
+		opts = {}
+	},
+	{
+		"benlubas/molten-nvim",
+		version = "^1.0.0",
+		build = ":UpdateRemotePlugins",
+		init = function()
+			vim.g.molten_output_win_max_height = 12
+		end,
+	},
+	{
 		"obsidian-nvim/obsidian.nvim",
 		version = "*", -- recommended, use latest release instead of latest commit
 		ft = "markdown",
