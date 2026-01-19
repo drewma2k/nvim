72d3381846be361f37393ea891cad04b5752f340
Drew Abram <drew.abram@gmail.com>
2026-01-19 08:07:41 -0500

    update blink snippets

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -4,10 +4,19 @@ return {
 		-- follow latest release.
 		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 		-- install jsregexp (optional!).
-		build = "make install_jsregexp"
+		-- build = "make install_jsregexp",
+		opts = {
+			region_check_events = {"CursorMoved", "InsertEnter"}
+		}
 	},
 	{
-		'honza/vim-snippets'
+		'honza/vim-snippets',
+		dependencies = {
+			'L3MON4D3/LuaSnip'
+		},
+		config = function ()
+			require("luasnip.loaders.from_snipmate").lazy_load()
+		end
 	},
 	{
 		'saghen/blink.cmp',
